5d2c3a284715b54dd1ecbd7d5bd78471da026a0a
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-04 22:56:33 +0200

    Ajout de la methode index du controller client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -13,72 +13,11 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //
+        $clients = Client::paginate(10); // Récupère les clients enregistrés et les pagine
+
+        return view('clients.index', compact('clients')); // Renvoie la vue index.blade.php avec les clients récupérés
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
