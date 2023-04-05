04f52a16924aad0fb9d0e8f802ab8e12435f7ebd
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-05 10:53:33 +0200

    Implementation de la vue show

--- resources/views/client/show.blade.php
+++ resources/views/client/show.blade.php
@@ -1,1 +1,18 @@
-<?php
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ $item->title }}</div>
+
+                    <div class="card-body">
+                        <p>{{ $item->description }}</p>
+                        <p>Price: {{ $item->price }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
